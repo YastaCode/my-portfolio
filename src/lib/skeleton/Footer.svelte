92e013c184fce7b99744857268c5ec9b337fbe70
Jesus Cordova <jescordova@hotmail.com>
2025-09-18 20:53:29 +0200

    Creation Footer component

---
+++ src/lib/skeleton/Footer.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<footer>
+    <p class="mt-5">
+        &copy; Tous les droits réservés | 
+        <a href="/" class="capitalize text-(--info)">yasta code</a>
+        2025
+    </p>
+</footer>
