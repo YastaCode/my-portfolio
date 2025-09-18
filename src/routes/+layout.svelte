e0b9727fc26f968f2b5ef9b0a4cacd81c156ae17
Jesus Cordova <jescordova@hotmail.com>
2025-09-18 17:00:30 +0200

    Add boxicons link and skeleton tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,14 @@
 	import favicon from '$lib/assets/favicon.svg';
 	
 	let { children } = $props();
+	
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </svelte:head>
 
-{@render children?.()}
+<div class="skeleton">
+	{@render children?.()}
+</div>
